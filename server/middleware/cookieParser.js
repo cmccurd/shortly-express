a5e74e81bfcf5173aabbe8460cc2236392c0bb63
Thomas Rose <thomascrose90@gmail.com>
2021-10-16 09:31:23 -0400

    Complete parsing cookie.

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,21 @@
+const session = require('../models/session.js');
+
 const parseCookies = (req, res, next) => {
+  var cookies = req.get('Cookie') || '';
+  var splitObj = {};
+  if (cookies === '') {
+    req.cookies = splitObj;
+  } else {
+    cookies = cookies.split('; ');
+    cookies.forEach((item) => {
+      var temp1 = item.split('=');
+      splitObj[temp1[0]] = temp1[1];
+    });
+
+    req.cookies = splitObj;
+
+  }
+  next();
 };
 
 module.exports = parseCookies;
